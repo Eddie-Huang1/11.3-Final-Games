115a67d496318cd09d0ae884710f229cddaeeddb
Eddie Huang <184601@cloud.rangitoto.school.nz>
2026-09-09 00:47:57 +0000

    new

--- Paperscissorsrock.py
+++ Paperscissorsrock.py
@@ -1,7 +1,7 @@
 """Paper scissors rock."""
 # A code to be able to randomize stuff
 import random
-# Variables 
+# Variables
 high_score = 0
 userscore = 0
 robotscore = 0
@@ -10,14 +10,14 @@ symbol_dictionary = ["paper", "scissors", "rock"]
 print("Lets play paper scissors rock, best out of three")
 print("You will be versing against the computer")
 print("Choose a symbol")
-symbol = input("")
-if symbol.lower() == "paper":
+player_symbol = input("")
+if player_symbol.lower() == "paper":
     print("ROCK")
     print("PAPER")
     print("SCISSORS")
     print("SHOOT")
 # This random input makes the computer randomly chooses between paper, scissors or rock
-random_symbol = random.choice(symbol_dictionary)
-print(random_symbol)
-print(symbol)
-print(f'{symbol} vs {random_symbol}')
+computer_symbol = random.choice(symbol_dictionary)
+print(f'{player_symbol} vs {computer_symbol}')
+if player_symbol == computer_symbol:
+    print("Tie")
