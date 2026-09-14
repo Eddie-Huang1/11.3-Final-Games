bd00713989e28d4ff22fc46cbe1ba789c0e07eb2
Eddie Huang <184601@cloud.rangitoto.school.nz>
2026-09-14 21:57:35 +0000

    github

--- Paperscissorsrock.py
+++ Paperscissorsrock.py
@@ -5,19 +5,28 @@ import random
 high_score = 0
 userscore = 0
 robotscore = 0
-symbol_dictionary = ["paper", "scissors", "rock"]
+replay = "yes"
+words = ["paper", "scissors", "rock"]
 
 print("Lets play paper scissors rock, best out of three")
 print("You will be versing against the computer")
 print("Choose a symbol")
 player_symbol = input("")
-if player_symbol.lower() == "paper":
+if player_symbol.lower() in words:
     print("ROCK")
     print("PAPER")
     print("SCISSORS")
     print("SHOOT")
 # This random input makes the computer randomly chooses between paper, scissors or rock
-computer_symbol = random.choice(symbol_dictionary)
+computer_symbol = random.choice(words)
 print(f'{player_symbol} vs {computer_symbol}')
 if player_symbol == computer_symbol:
     print("Tie")
+elif (player_symbol == "paper" and computer_symbol == "rock") or \
+(player_symbol == "scissors" and computer_symbol == "paper") or \
+(player_symbol == "rock" and computer_symbol == "scissors"):
+    print("You win!")
+    userscore += 1
+else:
+    print("You lose")
+    robotscore += 1
