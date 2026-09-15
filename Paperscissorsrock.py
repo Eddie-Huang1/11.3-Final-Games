6fdc03d0bc907e05f1ee0bb10dadd99f5ab26786
Eddie Huang <184601@cloud.rangitoto.school.nz>
2026-09-15 23:02:42 +0000

    broken

--- Paperscissorsrock.py
+++ Paperscissorsrock.py
@@ -5,18 +5,29 @@ import random
 high_score = 0
 userscore = 0
 robotscore = 0
+round_number = 1
 replay = "yes"
 words = ["paper", "scissors", "rock"]
 
-print("Lets play paper scissors rock, best out of three")
-print("You will be versing against the computer")
-print("Choose a symbol")
-player_symbol = input("")
-if player_symbol.lower() in words:
-    print("ROCK")
-    print("PAPER")
-    print("SCISSORS")
-    print("SHOOT")
+while replay.lower() == "yes":
+    print("Lets play paper scissors rock, best out of three")
+    print("You will be versing against the computer")
+
+    userscore = 0
+    robotscore = 0
+    round_number = 1
+
+    while round_number <= 3:
+        print(f"Round number: {round_number}")
+        print("Choose a symbol")
+        player_symbol = input("").lower()
+
+
+        if player_symbol.lower() in words:
+            print("ROCK")
+            print("PAPER")
+            print("SCISSORS")
+            print("SHOOT")
 # This random input makes the computer randomly chooses between paper, scissors or rock
 computer_symbol = random.choice(words)
 print(f'{player_symbol} vs {computer_symbol}')
@@ -30,3 +41,6 @@ elif (player_symbol == "paper" and computer_symbol == "rock") or \
 else:
     print("You lose")
     robotscore += 1
+
+print(f'Your score {userscore} vs Computer score {robotscore}')
+round_number += 1
